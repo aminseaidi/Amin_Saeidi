cdb702b35e46cca4b979a55d96a45f8eba362a67
aminseaidi <madmaminsaeidi1391@gmail.com>
2026-09-08 07:37:01 -0700

    Add files via upload
    salam

--- miniweb.py
+++ miniweb.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return '<H1> hello every body ! <H1>'
+    return '<H1>.سلام من محمد امین سعیدی هستم 15 ساله از ارومیه<H1>'
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    
